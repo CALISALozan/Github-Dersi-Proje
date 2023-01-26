922b3eef93a3ead15c0c174e9d247c0b0247c683
github_CALISALozan <ozanca1407@gmail.com>
2023-01-26 16:53:36 +0300

    ekleme

--- src/kisisel/deneme.java
+++ src/kisisel/deneme.java
@@ -3,6 +3,7 @@ package kisisel;
 public class deneme {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("Yalan Dunya");
     }
 
 }
